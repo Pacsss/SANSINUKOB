082d73f3ca58c7e45fe99fb3cd244dedc3c141dc
seftawingan <janjosefliza.tawingan25@bicol-u.edu.ph>
2025-09-25 00:00:36 +0800

    with working ui, subject to change

---
+++ assets/Scripts/touch_control.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+@onready var right: TouchScreenButton = $Right
+@onready var jump: TouchScreenButton = $Jump
+@onready var left: TouchScreenButton = $Left
+
+
+
+func _on_left_pressed() -> void:
+	left.modulate.a = 0.5
+
+func _on_left_released() -> void:
+	left.modulate.a = 1.0
+
+func _on_right_pressed() -> void:
+	right.modulate.a = 0.5
+
+func _on_right_released() -> void:
+	right.modulate.a = 1.0
+
+func _on_jump_pressed() -> void:
+	jump.modulate.a = 0.5
+
+func _on_jump_released() -> void:
+	jump.modulate.a = 1.0
