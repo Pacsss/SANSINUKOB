4bd2f6626003797d89b3b01c1dd664e7141cfb72
seftawingan <janjosefliza.tawingan25@bicol-u.edu.ph>
2025-09-28 00:55:08 +0800

    time speed fixed

--- scripts/systems/days.gd
+++ scripts/systems/days.gd
@@ -3,13 +3,13 @@ extends Control
 @onready var sol_label: Label = $DayPanel/MarginContainer/SolLabel
 @onready var time_label: Label = $TimePanel/MarginContainer/TimeLabel
 
-@export var normal_speed = 2
+@export var normal_speed = 0.1
 
 func _ready() -> void:
 	GameTimer.time_tick.connect(on_time_tick)
 
-func on_time_tick(sol: int, hour: int, minute: int, second: float) -> void:
+func on_time_tick(sol: int, hour: int, minute: int) -> void:
 	sol_label.text = "Sol " + str(sol)
-	time_label.text = "%02d:%02d:%02f" % [hour, minute, second]
+	time_label.text = "%02d:%02d" % [hour, minute]
 	
 	
