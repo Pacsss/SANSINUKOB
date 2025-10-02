670159506a677868353cf7445d766e640f648d97
seftawingan <janjosefliza.tawingan25@bicol-u.edu.ph>
2025-10-02 22:55:30 +0800

    with clickable menu helmet/book/folder

--- scripts/systems/menu.gd
+++ scripts/systems/menu.gd
@@ -2,26 +2,47 @@ extends Control
 @onready var helmet: TouchScreenButton = $helmet/helmet
 @onready var folder: TouchScreenButton = $folder/folder
 @onready var book: TouchScreenButton = $book/book
-@onready var canvas_layer: CanvasLayer = $CanvasLayer
+@onready var helmetcanvas: CanvasLayer = $helmetcanvas
+@onready var foldercanvas: CanvasLayer = $foldercanvas
+@onready var bookcanvas: CanvasLayer = $bookcanvas
 
 func _ready():
-	canvas_layer.visible = false
+	close_all_menus()
+
 
 func _on_helmet_pressed() -> void:
+	close_all_menus()
 	helmet.modulate.a = 0.5
-	canvas_layer.visible = not canvas_layer.visible
+	helmetcanvas.visible = true 
+
 
 func _on_helmet_released() -> void:
 	helmet.modulate.a = 1.0
 
+
 func _on_folder_pressed() -> void:
+	close_all_menus()
 	folder.modulate.a = 0.5
+	foldercanvas.visible = true
+
 
 func _on_folder_released() -> void:
 	folder.modulate.a = 1.0
 
+
 func _on_book_pressed() -> void:
+	close_all_menus()
 	book.modulate.a = 0.5
+	bookcanvas.visible = true
+
 
 func _on_book_released() -> void:
 	book.modulate.a = 1.0
+
+func _on_close_pressed() -> void:
+	close_all_menus()
+
+func close_all_menus():
+	helmetcanvas.visible = false
+	foldercanvas.visible = false
+	bookcanvas.visible = false
